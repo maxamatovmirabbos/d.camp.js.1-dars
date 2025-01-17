d7341275b5dd5b23223fca04b8dead20c277e3b4
Mirabbos <https://github.com/maxamatovmirabbos>
2025-01-17 20:55:24 +0500

    hatolar tuzatildi

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+ism = prompt("ismingiz nima ")
+familiya = prompt("familiyangizni kiriting")
+yosh = prompt("yoshingiz nechida")
+ish = prompt("ish joyingiz qayerda")
+
+document.write(` ${ism}, ${familiya}, ${yosh}, ${ish} `);
+
+
+// document.write(' ${ism}, ${familiya}, ${yosh}, ${ish} ');
